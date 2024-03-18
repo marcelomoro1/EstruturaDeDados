41c8f84cadb26b21da6c7e2552096d5f07a5bae5
Marcelo Moro <101741773+marcelomoro1@users.noreply.github.com>
2024-03-18 20:02:47 -0300

    Add files via upload

---
+++ Listas/ListaCsharp/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿void exibir(List<int> lista)
+{
+    foreach(var item in lista)
+    {
+        Console.WriteLine(item);
+    }
+}
+
+
+List<int> lista = new List<int>();
+int qtdnumeros = 10;
+
+Random gerador = new Random();
+int valor;
+for(int i =0; i < qtdnumeros; i++)
+{
+    valor = gerador.Next(100);
+
+    if (!lista.Contains(valor))
+    {
+        lista.Add(valor);
+        lista.Sort();
+    }
+
+}
+Console.WriteLine("Tamanho da lista " + lista.Count);
+exibir(lista);
+
+Console.Write("Digite um valor para exclusão: ");
+valor = int.Parse(Console.ReadLine());
+lista.Remove(valor);
+
+Console.WriteLine("Tamanho da lista " + lista.Count);
+exibir(lista);
